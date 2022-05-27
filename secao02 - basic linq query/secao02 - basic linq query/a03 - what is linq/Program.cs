1afe66cedeb80f4b0ecbb8bf4bac0fa7eebfcacf
Ciro Watanabe <cw.artiga@gmail.com>
2022-05-26 22:11:14 -0300

    Criacao do repositório Linq course udemy

---
+++ secao02 - basic linq query/secao02 - basic linq query/a03 - what is linq/Program.cs
@@ -0,0 +1,20 @@
+﻿string sentence = "I love cats";
+string[] catNames = { "Lucky", "Bella", "Luna", "Oreo", "Simba", "Toby", "Loki", "Oscar" };
+int[] numbers = { 5, 6, 3, 2, 1, 5, 6, 7, 8, 4, 234, 54, 14, 653, 3, 4, 5, 6, 7 };
+
+var getTheNumbers = from number in numbers
+                    where number < 5
+                    select number;
+
+List<int> newNumbers = new List<int>();
+
+foreach (var number in numbers)
+{
+    if (number < 5)
+    {
+        newNumbers.Add(number);
+    }
+}
+
+Console.WriteLine(string.Join(", ", getTheNumbers));
+Console.WriteLine(string.Join(", ", newNumbers));
