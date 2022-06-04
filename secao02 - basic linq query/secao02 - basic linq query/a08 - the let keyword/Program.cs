24b1ef8b8c1f76b33e13c005888ca8cb2f9691e8
Ciro Watanabe <cw.artiga@gmail.com>
2022-06-04 13:18:31 -0300

    a08 - the let keyword

---
+++ secao02 - basic linq query/secao02 - basic linq query/a08 - the let keyword/Program.cs
@@ -0,0 +1,120 @@
+﻿List<Person> people = new List<Person>()
+{
+    new Person("Tod", "Vachev", 1, 180, 26, Gender.Male),
+    new Person("John", "Johnson", 2, 170, 21, Gender.Male),
+    new Person("Anna", "Maria", 3, 150, 22, Gender.Female),
+    new Person("Kyle", "Wilson", 4, 164, 29, Gender.Male),
+    new Person("Anna", "Williams", 5, 164, 28, Gender.Male),
+    new Person("Maria", "Ann", 6, 160, 43, Gender.Female),
+    new Person("John", "Jones", 7, 160, 37, Gender.Female),
+    new Person("Samba", "TheLion", 8, 175, 33, Gender.Male),
+    new Person("Aaron", "Myers", 9, 182, 21, Gender.Male),
+    new Person("Aby", "Wood", 10, 165, 20, Gender.Female),
+    new Person("Maddie","Lewis",  11, 160, 19, Gender.Female),
+    new Person("Lara", "Croft", 12, 162, 18, Gender.Female)
+};
+
+
+
+var peopleWithA = from p in people
+                  let firstChar = p.FirstName.ToLower()[0]
+                  where firstChar == 'a'
+                  let fullName = string.Format($"{p.FirstName} {p.LastName}")
+                  select new YoungPerson { FullName = fullName, Age = p.Age};
+
+
+foreach (var p in peopleWithA)
+{
+    Console.WriteLine(p.FullName);
+}
+
+
+
+
+
+List<List<int>> lists = new List<List<int>>()
+{
+    new List<int>() { 1, 2, 3 },
+    new List<int>() { 4, 5, 6 },
+    new List<int>() { 7, 8, 9 }
+};
+
+var allNumbers = from l in lists
+                 let numbers = l
+                 from n in numbers
+                 let squaredNumber = n * n
+                 where squaredNumber < 50
+                 select squaredNumber;
+
+foreach (var n in allNumbers)
+{
+    Console.WriteLine(n);
+}
+
+
+
+
+
+
+internal class YoungPerson
+{
+    public string FullName { get; set; }
+    public int Age { get; set; }
+}
+
+internal class Person
+{
+    private string firstName;
+    private string lastName;
+    private int id;
+    private int height;
+    private int age;
+
+    private Gender gender;
+
+    public string FirstName
+    {
+        get { return firstName; }
+        set { firstName = value; }
+    }
+
+    public string LastName
+    {
+        get { return lastName; }
+        set { lastName = value; }
+    }
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public int Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Gender Gender { get; set; }
+
+    public Person(string firstName, string lastName, int id, int height, int age, Gender gender)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = id;
+        Height = height;
+        Gender = gender;
+        Age = age;
+    }
+}
+
+internal enum Gender
+{
+    Male,
+    Female
+}
