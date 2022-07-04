9d6fcf02b96b2e6d4d48658a5d265d6599a578d4
Ciro Watanabe <cw.artiga@gmail.com>
2022-07-04 12:26:04 -0300

    Aulas 18, 19, 20

---
+++ secao05 - joining LINQ queries/a19 - group join and inner group join/Program.cs
@@ -0,0 +1,58 @@
+﻿List<Buyer> buyers = new List<Buyer>()
+            {
+                new Buyer() { Name = "Johny", District = "Fantasy District", Age = 22},
+                new Buyer() { Name = "Peter", District = "Scientists District", Age = 40},
+                new Buyer() { Name = "Paul", District = "Fantasy District", Age = 30 },
+                new Buyer() { Name = "Maria", District = "Scientists District", Age = 35 },
+                new Buyer() { Name = "Joshua", District = "EarthIsFlat District", Age = 40 },
+                new Buyer() { Name = "Sylvia", District = "Developers District", Age = 22 },
+                new Buyer() { Name = "Rebecca", District = "Scientists District", Age = 30 },
+                new Buyer() { Name = "Jaime", District = "Developers District", Age = 35 },
+                new Buyer() { Name = "Pierce", District = "Fantasy District", Age = 40 }
+            };
+List<Supplier> suppliers = new List<Supplier>()
+            {
+                new Supplier() { Name = "Harrison", District = "Fantasy District", Age = 22 },
+                new Supplier() { Name = "Charles", District = "Developers District", Age = 40 },
+                new Supplier() { Name = "Hailee", District = "Scientists District", Age = 35 },
+                new Supplier() { Name = "Taylor", District = "EarthIsFlat District", Age = 30 }
+            };
+
+
+var groupJoin = from s in suppliers
+                join b in buyers on s.District equals b.District into buyersGroup // buyersGroup is IEnumerable
+                select new
+                {
+                    s.Name,
+                    s.District,
+                    Buyers = from b in buyersGroup
+                             orderby b.Age
+                             select b
+                    // Here we made another linq search to define the Buyers attribute. When we have a linq inside a group join, we have a inner group join
+                };
+
+foreach (var supplier in groupJoin)
+{
+    Console.WriteLine($"Supplier: {supplier.Name}; District: {supplier.District}");
+    foreach (var buyer in supplier.Buyers)
+    {
+        Console.WriteLine($"    {buyer.Name}, {buyer.Age}");
+    }
+}
+
+
+
+internal class Supplier
+{
+    public string Name { get; set; }
+    public string District { get; set; }
+    public int Age { get; set; }
+}
+
+internal class Buyer
+{
+    public string Name { get; set; }
+    public string District { get; set; }
+    public int Age { get; set; }
+}
+
---
+++ secao05 - joining LINQ queries/a20 - left outer join/Program.cs
@@ -0,0 +1,80 @@
+﻿List<Buyer> buyers = new List<Buyer>()
+            {
+                new Buyer() { Name = "Johny", District = "Fantasy District", Age = 22},
+                new Buyer() { Name = "Peter", District = "Scientists District", Age = 40},
+                new Buyer() { Name = "Paul", District = "Fantasy District", Age = 30 },
+                new Buyer() { Name = "Maria", District = "Scientists District", Age = 35 },
+                new Buyer() { Name = "Joshua", District = "Developers District", Age = 40 },
+                new Buyer() { Name = "Sylvia", District = "Developers District", Age = 22 },
+                new Buyer() { Name = "Rebecca", District = "Scientists District", Age = 30 },
+                new Buyer() { Name = "Jaime", District = "Developers District", Age = 35 },
+                new Buyer() { Name = "Pierce", District = "Fantasy District", Age = 40 }
+            };
+List<Supplier> suppliers = new List<Supplier>()
+            {
+                new Supplier() { Name = "Harrison", District = "Fantasy District", Age = 22 },
+                new Supplier() { Name = "Charles", District = "Developers District", Age = 40 },
+                new Supplier() { Name = "Hailee", District = "Scientists District", Age = 35 },
+                new Supplier() { Name = "Taylor", District = "EarthIsFlat District", Age = 30 }
+            };
+
+
+var leftOuterJoin = from s in suppliers
+                    join b in buyers on s.District equals b.District into buyersGroup
+                    select new
+                    {
+                        s.Name,
+                        s.District,
+                        Buyers = buyersGroup.DefaultIfEmpty(
+                        new Buyer()
+                        {
+                            Name = "No one here",
+                            District = "I dont exist!"
+                        })
+                    };
+
+foreach (var item in leftOuterJoin)
+{
+    Console.WriteLine($"Supplier: {item.Name}, District: {item.District}");
+    foreach (var buyer in item.Buyers)
+    {
+        Console.WriteLine($"        {buyer.Name}");
+    }
+}
+
+
+// There is another way to make the left outer join
+Console.WriteLine("===================================");
+
+var alternativeLeftOuterJoin = from s in suppliers
+                               join b in buyers on s.District equals b.District into buyersGroup
+                               from bG in buyersGroup.DefaultIfEmpty()
+                               select new
+                               {
+                                   s.Name,
+                                   s.District,
+                                   BuyersName = bG?.Name ?? "No one here",
+                                   BuyersDistrict = bG?.District ?? "I dont exist"
+                               };
+
+foreach (var item in alternativeLeftOuterJoin)
+{
+    Console.WriteLine($"{item.Name} - {item.District}");
+    Console.WriteLine($"    {item.BuyersName}, {item.BuyersDistrict}");
+}
+
+
+internal class Supplier
+{
+    public string Name { get; set; }
+    public string District { get; set; }
+    public int Age { get; set; }
+}
+
+internal class Buyer
+{
+    public string Name { get; set; }
+    public string District { get; set; }
+    public int Age { get; set; }
+}
+
